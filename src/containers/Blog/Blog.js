82ee148ff2e8b9c83c55a0568376781df9d83e54
Minh Mai <minhmanmai90@gmail.com>
2020-01-31 16:48:03 +1100

    fetch post data using http request and promise

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,33 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+    state = {
+        posts: []
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Minh'
+                    }
+                })
+                this.setState({
+                    posts: updatedPosts
+                })
+                console.log(response);
+            });
+    }
+    render() {
+        const posts = this.state.posts.map(post => {
+            return <Post title={post.title} author={post.author} key={post.id} />
+        });
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
